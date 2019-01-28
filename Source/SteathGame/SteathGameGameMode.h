26a5197c85c256e878ba7df53780dbb7e354bf45
Mit <mit.doshi557@gmail.com>
2019-01-27 23:20:49 -0700

    Initial commit

---
+++ Source/SteathGame/SteathGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SteathGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASteathGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASteathGameGameMode();
+};
+
+
+
